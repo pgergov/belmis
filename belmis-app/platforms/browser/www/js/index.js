f39e671ebee1fdd34244d0bbaa9ae9097263dbb4
Ivo-Donchev <ivodonchev267@gmail.com>
2016-10-01 13:30:29 +0300

    Confuguring app

--- belmis-app/platforms/browser/www/js/index.js
+++ belmis-app/platforms/browser/www/js/index.js
@@ -35,44 +35,59 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
         console.log('device ready')
-        window.wakeuptimer.wakeup( successCallback,  
-           errorCallback, 
-           // a list of alarms to set
-           {
-                alarms : [{
-                    type : 'onetime',
-                    time : { hour : 14, minute : 30 },
-                    extra : { message : 'json containing app-specific information to be posted when alarm triggers' }, 
-                    message : 'Alarm has expired!'
-               }] 
-           }
-        );
+        $('.alarm-hour').timepicker({ 'timeFormat': 'H:i:s' });
 
-        // snooze...
-        window.wakeuptimer.snooze( successCallback,
-            errorCallback,
-            {
-                alarms : [{
-                    type : 'snooze',
-                    time : { seconds : 60 }, // snooze for 60 seconds 
-                    extra : { }, // json containing app-specific information to be posted when alarm triggers
-                    message : this.get('message'),
-                    sound : this.get('sound'),
-                    action : this.get('action')
-                }]
-            }
-         );
+        $('.set-alarm').click(function(){
+            console.log($('.alarm-hour').val())
+            window.wakeuptimer.wakeup( successCallback,  
+               errorCallback, 
+               // a list of alarms to set
+               {
+                    alarms : [{
+                        type : 'onetime',
+                        time : { hour : parseInt($('.alarm-hour').val().split(':')[0]), minute : parseInt($('.alarm-hour').val().split(':')[1]) },
+                        extra : { message : 'json containing app-specific information to be posted when alarm triggers' }, 
+                        message : 'Alarm has expired!'
+                   }] 
+               }
+            );
 
-        // example of a callback method
-        var successCallback = function(result) {
-            if (result.type==='wakeup') {
-                console.log('wakeup alarm detected--' + result.extra);
-            } else if(result.type==='set'){
-                console.log('wakeup alarm set--' + result);
-            } else {
-                console.log('wakeup unhandled type (' + result.type + ')');
-            }
-        }; 
+            // // snooze...
+            // window.wakeuptimer.snooze( successCallback,
+            //     errorCallback,
+            //     {
+            //         alarms : [{
+            //             type : 'snooze',
+            //             time : { seconds : 60 }, // snooze for 60 seconds 
+            //             extra : { message : 'json containing app-specific information to be posted when alarm triggers' }, 
+            //             message : 'Alarm has snoozed!'
+            //         }]
+            //     }
+            //  );
+
+            // example of a callback method
+            var successCallback = function(result) {
+                navigator.notification.alert(result.time)
+                if (result.type==='wakeup') {
+                    console.log('wakeup alarm detected--' + result.extra);
+                } else if(result.type==='set'){
+                    console.log('wakeup alarm set--' + result);
+                } else {
+                    console.log('wakeup unhandled type (' + result.type + ')');
+                }
+            }; 
+
+            var errorCallback = function(result) {
+                console.log(result)
+                // if (result.type==='wakeup') {
+                //     console.log('wakeup alarm detected--' + result.extra);
+                // } else if(result.type==='set'){
+                //     console.log('wakeup alarm set--' + result);
+                // } else {
+                //     console.log('wakeup unhandled type (' + result.type + ')');
+                // }
+            }; 
+        })
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
